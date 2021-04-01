4f02f90362cc2b81b5cb5dfe35a9ba1e7d652869
Dolgmen <slotsy046@gmail.com>
2021-04-01 15:23:55 +0300

    Домашнє 1 rev 3

--- Home_work1.rb
+++ Home_work1.rb
@@ -2,7 +2,22 @@ puts "Hey word"
 small = 56451
 medium = 8564
 mckey = (small + medium)
+puts mckey
 mckey = (small - medium)
+puts mckey
 mckey = (small * medium)
+puts mckey
 mckey = (small / medium)
-print mckey
+puts mckey
+pie = ("lemon" + "3.14")
+puts pie = 3.14
+number = 3
+puts number
+number += 2
+puts number
+number *= 2
+puts number
+
+string = "ad"
+string += "gh"
+puts string
