44c58ec17b665f6f146836d15533bc2587dbd361
Dolgmen <slotsy046@gmail.com>
2021-04-01 18:24:09 +0300

    Гра вер 3

--- get_numbers.rb
+++ get_numbers.rb
@@ -7,6 +7,8 @@ puts "Вітаю, #{name} !"
 puts "Компютер загадав випадкове число від 0 до 100."
 puts "Спробуєш відгадати ?"
 target = rand (100) +1
+puts "Ти маєш #{10}"
 
-
+print "Make a guess: "
+guess = gets.to_i
 
